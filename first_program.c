e6a92a2c7c5d498c989218f54c1497fe11de135d
Arthur <31354771+FrenkiArt@users.noreply.github.com>
2020-09-29 13:55:59 +0300

    added my C files
    first programm

---
+++ first_program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("--------------start----------------------\n\n");
+	printf("My first program in C language!\n");
+	printf("Моя первыая программа на языке Си!\n");
+	printf("\n---------------end-----------------------\n\n");
+	// Просто комментарий
+	/*
+	*	Многострочный комментарий
+	*/
+	return (0);
+}
