23d70e29548d71d14fdd71aec4358e1fddad71a7
Thrace Ilda <thilda@et-f4.kzn.21-school.ru>
2020-09-29 14:16:28 +0300

    varables

---
+++ second_program.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+int main(void)
+{
+    int myVar = 10;
+    int number;
+    int year, month, day;
+    char letter = 'A';
+    int number1 = 99;
+    
+    number = 5;
+    year = 1986, month = 6, day = 30;
+    return (0);
+}
